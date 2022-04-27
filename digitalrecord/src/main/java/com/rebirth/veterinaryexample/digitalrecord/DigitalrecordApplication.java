691931cc3c56f928f902b1e7f2c1948f099cfded
Roberto Cazarin <robertodmngzczrn@gmail.com>
2022-04-26 19:27:57 -0500

    Welcome! Digital Records
    Nuevo modulo de la aplicación. para gestionar los documentos, recuperarlos y generar reportes de estos.

---
+++ digitalrecord/src/main/java/com/rebirth/veterinaryexample/digitalrecord/DigitalrecordApplication.java
@@ -0,0 +1,13 @@
+package com.rebirth.veterinaryexample.digitalrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalrecordApplication.class, args);
+    }
+
+}
---
+++ digitalrecord/src/test/java/com/rebirth/veterinaryexample/digitalrecord/DigitalrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebirth.veterinaryexample.digitalrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalrecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
